30c320d416f182215bcec1acef6d47d0e9a4b9bf
nathanael gracedo <nathanjuan56@gmail.com>
2023-09-27 15:22:33 +0700

    tugas jobsheet 5

---
+++ LatihanIndividu2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LatihanIndividu2 {
+
+    public static void main(String[] args) {
+        String username = "user123";
+        String password = "pass123";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan username: ");
+        String inputUsername = scanner.nextLine();
+        System.out.print("Masukkan password: ");
+        String inputPassword = scanner.nextLine();
+
+      
+        if (inputUsername.equals(username) && inputPassword.equals(password)) {
+            System.out.println("Login berhasil!");
+        } else {
+            System.out.println("Login gagal. Username atau password salah.");
+        }
+        
+        scanner.close();
+    }
+}
