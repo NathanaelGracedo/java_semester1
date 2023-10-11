766df5f45895875cc26b228d3ed7a2edb2283e3d
nathanael gracedo <nathanjuan56@gmail.com>
2023-10-11 13:49:38 +0700

    Initial commit

---
+++ src/doWhileCuti21.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+public class doWhileCuti21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String Konfirmasi;
+
+        System.out.print("Jatah Cuti: ");
+        jatahCuti = sc.nextInt();
+
+        do{
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            Konfirmasi = sc.next();
+
+            if (Konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah Hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti );
+                } else {
+                    System.out.println("Sisa jatah cuti anda tidak mencukupi");
+                    break;
+                }
+            }
+        } while (jatahCuti > 0 );
+
+        sc.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
