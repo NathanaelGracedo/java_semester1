5b646f1608aa78f08bdf13e6337abaa8ff36cf85
NathanaelGracedo <nathanjuan56@gmail.com>
2023-10-25 15:34:21 +0700

    modifikasi ArrayRataNilai

--- jobSheet9/src/ArrayRataNilai21.java
+++ jobSheet9/src/ArrayRataNilai21.java
@@ -2,18 +2,36 @@ import java.util.Scanner;
 public class ArrayRataNilai21 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
+        int[] nilaiMhs;
         double total = 0;
-        double rata2;
+        int lulus = 0;
+        int tdklulus = 0;
+        int jmlhNilaiLulus = 0;
+        double ratanilaiLulus = 0;
+        double ratanilaitdkLulus = 0;
 
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int n = input.nextInt();
+        nilaiMhs = new int[n];
+        
         for (int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = input.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                jmlhNilaiLulus += nilaiMhs[i];
+                lulus++;
+            }else tdklulus++;
         }
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
+        
+        ratanilaiLulus = jmlhNilaiLulus / lulus;
+        ratanilaitdkLulus = (total - jmlhNilaiLulus) / tdklulus;
+        System.out.println("Rata-rata nilai mahasiswa yang lulus = " + ratanilaiLulus );
+        System.out.println("Rata-rata nilai mahasiswa yang tidak lulus = " + ratanilaitdkLulus);
+        // System.out.println("Jumlah mahasiswa yang lulus adalah "+ lulus);
+        
+        input.close();
     }
 }
