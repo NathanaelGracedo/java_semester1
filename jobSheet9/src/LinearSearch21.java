8f483271361f399f3372d17930dd5c8934876a61
NathanaelGracedo <nathanjuan56@gmail.com>
2023-10-26 10:19:19 +0700

    modifikasi LienarSearch

--- jobSheet9/src/LinearSearch21.java
+++ jobSheet9/src/LinearSearch21.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class LinearSearch21 {
     public static void main(String[] args) {
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        Scanner input = new Scanner(System.in);
+        int[] arrayInt;
+        int key = 0;
         int hasil = 0;
+
+        System.out.print("Masukkan jumlah elemen array : ");
+        int n = input.nextInt();
+        arrayInt = new int[n];
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan nilai elemen array ke-" + i + " : ");
+            arrayInt[i] = input.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari :");
+        key = input.nextInt();
 
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
@@ -10,7 +25,11 @@ public class LinearSearch21 {
                 break;
             }
         }
-
-        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+        if (hasil != 0) {
+            System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+        } else {
+            System.out.println("Key tidak ditemukan");
+        }
+        input.close();
     }
 }
