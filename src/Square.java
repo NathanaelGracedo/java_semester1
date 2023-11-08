2292acaec755b016d32359ed70ce23363807f4d0
NathanaelGracedo <nathanjuan56@gmail.com>
2023-11-08 13:22:20 +0700

    Modifikasi percobaan 2

--- src/Square.java
+++ src/Square.java
@@ -6,10 +6,10 @@ public class Square {
         int N = sc.nextInt();
 
         for (int i = 1; i <=N; i++) {
-            for (int j = 1; j <=N; j++) {
+            for (int j = 0; j <=N; j++) {
                 System.out.print("*");
             }
-            System.out.println();
+           
         }
         sc.close();
     }
