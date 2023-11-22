a020c85e433c519f09c72057f0741ae1183ed249
NathanaelGracedo <nathanjuan56@gmail.com>
2023-11-22 12:43:11 +0700

    percobaan 2

--- src/terimaKasih_21.java
+++ src/terimaKasih_21.java
@@ -2,8 +2,12 @@ public class terimaKasih_21 {
     static void ucapanTerimaKasih(){
         System.out.println("Thank you for being best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like i could ask you anithing.");
     }
+    public static void UcapanTambahan(String ucapan){
+        System.out.println(ucapan);
+    }
     public static void main(String[] args) throws Exception {
         ucapanTerimaKasih();
-        System.out.println("Thank you for being best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like i could ask you anithing.");
+        String ucapan = "Terima Kasih Pak... Bu... Semoga sehat selalu";
+        UcapanTambahan(ucapan);
     }
 }
