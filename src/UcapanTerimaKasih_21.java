6f41c20e85df9772203b10ca37f8cbad32a914e6
NathanaelGracedo <nathanjuan56@gmail.com>
2023-11-22 14:54:48 +0700

    percobaan 4 modif

--- src/UcapanTerimaKasih_21.java
+++ src/UcapanTerimaKasih_21.java
@@ -9,15 +9,15 @@ public class UcapanTerimaKasih_21 {
     }
     public static void UcapanTerimakasih(){
         String nama = PenerimaUcapan();
-        String tambahan = UcapanTambahan();
         System.out.println("Thank you " + nama + " for being the best teacher in the world.\n"+
-        "You inspired in me a love for learning and made me feel like i could ask you anithing.\n"+ tambahan);
+        "You inspired in me a love for learning and made me feel like i could ask you anithing.");
     }
-    public static String UcapanTambahan(){
-        String tambahan = "Wish all the best for you.";
-        return tambahan;
+    public static void UcapanTambahan(String ucapan){
+        System.out.println(ucapan);
     }
     public static void main(String[] args) {
         UcapanTerimakasih();
+        String ucapan = "Wish you all the best";
+        UcapanTambahan(ucapan);
     }
 }
