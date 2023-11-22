cb0ceafd41742adc6a502aad59026729c1b4fce9
NathanaelGracedo <nathanjuan56@gmail.com>
2023-11-22 12:28:03 +0700

    percobaan 1

---
+++ src/terimaKasih_21.java
@@ -0,0 +1,9 @@
+public class terimaKasih_21 {
+    static void ucapanTerimaKasih(){
+        System.out.println("Thank you for being best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like i could ask you anithing.");
+    }
+    public static void main(String[] args) throws Exception {
+        ucapanTerimaKasih();
+        System.out.println("Thank you for being best teacher in the world.\n" + "You inspired in me a love for learning and made me feel like i could ask you anithing.");
+    }
+}
