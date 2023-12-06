8272bb182e864ec5d6fd659604c81fa4418e00a9
NathanaelGracedo <nathanjuan56@gmail.com>
2023-12-06 14:48:24 +0700

    Modif

---
+++ src/deret1sampai50.java
@@ -0,0 +1,9 @@
+public class deret1sampai50 {
+public static void main(String[] args) {
+    for (int i = 1; i <= 50; i++) {
+        if (i % 3 != 0) {
+            System.out.print(i + " ");
+        }
+    }
+}
+}
---
+++ src/deretBil25.java
@@ -0,0 +1,12 @@
+public class deretBil25 {
+    public static void main(String[] args) {
+        int i = 25;
+        int total = 0;
+
+        while (i >= 1) {
+            total += i;
+            i--;
+        }
+        System.out.println("Total deret bilangan 25 : " + total);
+    }
+}
